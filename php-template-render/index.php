338ea9c1f6e2c99f4b1dfdbc697a946c8bfc2041
Eric Patrick <erstpa@gmail.com>
2017-10-31 17:50:16 -0400

    Added templating template

---
+++ php-template-render/index.php
@@ -0,0 +1,11 @@
+<?php require("helpers.php"); ?>
+
+<?php render("header", ["title" => "CS50"]); ?>
+
+<ul>
+	<li>Admiral</li>
+	<li>Betty</li>
+	<li>Chauncey</li>
+</ul>
+
+<?php render("footer"); ?>
